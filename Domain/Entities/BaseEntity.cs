6405d1b0cc94a85587dcc79a8c9a6dd4ca3a34ef
Sai Prabandh <saiprabandh2004@gmail.com>
2026-05-05 18:00:35 +0530

    Initial clean architecture setup with domain structure

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+```
+public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public byte[] RowVersion { get; set; } = default!;
+```
+
+}
+
+}
