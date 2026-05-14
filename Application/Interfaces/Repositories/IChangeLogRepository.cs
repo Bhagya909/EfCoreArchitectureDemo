cd95ec48e0efd13d6dce6c35d2e61e7fb6fd0fbf
Jogi Bhagyalaxmi <jogibhagyalaxmi55@gmail.com>
2026-05-14 11:29:25 +0530

    application layer completed

--- Application/Interfaces/Repositories/IChangeLogRepository.cs
+++ Application/Interfaces/Repositories/IChangeLogRepository.cs
@@ -7,6 +7,7 @@ namespace Application.Interfaces.Repositories
 {
     public interface IChangeLogRepository
     {
+        // change log repository interface
         Task AddAsync(ChangeLog changeLog);
 
     }
